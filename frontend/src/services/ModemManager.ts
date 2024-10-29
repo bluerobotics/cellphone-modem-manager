a12b3b18b44acfafda21a1852db25aa1e58f65f7
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-10-29 15:53:40 -0300

    frontend: Update models and service API calls
    * Update models and service API calls to match new implementation in
      backend

--- frontend/src/services/ModemManager.ts
+++ frontend/src/services/ModemManager.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { ModemDevice } from '@/types/ModemManager'
+import { ModemDevice, ModemSignalQuality, ModemCellInfo, PDPInfo } from '@/types/ModemManager'
 
 const MODEM_MANAGER_V1_API = `/v1.0`
 
@@ -9,46 +9,114 @@ const api = axios.create({
 
 /**
  * List device descriptor of all connected modems.
- * @returns {Promise<string[]>}
+ * @returns {Promise<ModemDevice[]>}
  */
 export async function fetchModems(): Promise<ModemDevice[]> {
   const response = await api.get('/modem', { timeout: 5000 })
-
   return response.data as ModemDevice[]
-
-  // return [
-  //   {
-  //     device: '/sys/devices/platform/scb/fd500000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0/usb1/1-1/1-1.1',
-  //     manufacturer: 'Quectel',
-  //     product: 'EC25-E',
-  //   },
-  //   {
-  //     device: '/sys/devices/platform/scb/fd500000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0/usb2/1-1/1-1.1',
-  //     manufacturer: 'Quectel',
-  //     product: 'EC25-E',
-  //   },
-  // ] as ModemDevice[]
 }
 
 /**
  * Get details of a modem by identifier.
- * @returns {Promise<ModemDevice[]>}
+ * @param {string} id - Modem ID
+ * @returns {Promise<ModemDevice>}
  */
-export async function fetchModemByDevice(device: string): Promise<ModemDevice> {
-  const response = await api.get(`/modem/${device}`, { timeout: 5000 })
-
+export async function fetchModemByDevice(id: string): Promise<ModemDevice> {
+  const response = await api.get(`/modem/${id}/details`, { timeout: 5000 })
   return response.data as ModemDevice
 }
 
+/**
+ * Get signal strength of a modem by device id.
+ * @param {string} id - Modem ID
+ * @returns {Promise<ModemSignalQuality>}
+ */
+export async function fetchSignalStrength(id: string): Promise<ModemSignalQuality> {
+  const response = await api.get(`/modem/${id}/signal`, { timeout: 5000 })
+  return response.data as ModemSignalQuality
+}
+
+/**
+ * Get serving cell and neighbors cells information of a modem by device id.
+ * @param {string} id - Modem ID
+ * @returns {Promise<ModemCellInfo>}
+ */
+export async function fetchServingCellInfo(id: string): Promise<ModemCellInfo> {
+  const response = await api.get(`/modem/${id}/cell`, { timeout: 5000 })
+  return response.data as ModemCellInfo
+}
+
+/**
+ * Reboot a modem by device id.
+ * @param {string} id - Modem ID
+ * @returns {Promise<void>}
+ */
+export async function rebootModem(id: string): Promise<void> {
+  await api.post(`/modem/${id}/reboot`, null, { timeout: 5000 })
+}
+
+/**
+ * Get USB mode of a modem by device.
+ * @param {string} id - Modem ID
+ * @returns {Promise<number>}
+ */
+export async function fetchUSBMode(id: string): Promise<number> {
+  const response = await api.get(`/modem/${id}/config/usb_net`, { timeout: 5000 })
+  return response.data as number
+}
+
+/**
+ * Set USB mode of a modem by device.
+ * @param {string} id - Modem ID
+ * @param {string} mode - New USB mode
+ * @returns {Promise<void>}
+ */
+export async function setUSBMode(id: string, mode: string): Promise<void> {
+  await api.put(`/modem/${id}/config/usb_net/${mode}`, null, { timeout: 5000 })
+}
+
+/**
+ * Get PDP information of a modem by device.
+ * @param {string} id - Modem ID
+ * @returns {Promise<PDPInfo[]>}
+ */
+export async function fetchPDPInfo(id: string): Promise<PDPInfo[]> {
+  const response = await api.get(`/modem/${id}/pdp`, { timeout: 5000 })
+  return response.data as PDPInfo[]
+}
+
+/**
+ * Set APN of a profile of a modem by device.
+ * @param {string} id - Modem ID
+ * @param {number} profile - Profile ID
+ * @param {string} apn - APN to set
+ * @returns {Promise<void>}
+ */
+export async function setAPNByProfile(id: string, profile: number, apn: string): Promise<void> {
+  await api.put(`/modem/${id}/pdp/${profile}apn/${apn}`, null, { timeout: 5000 })
+}
+
+/**
+ * Ping a server by modem device ID, be careful that this can take a long time, and serial is blocked. So, other
+ * requests will be blocked until this request is finished.
+ * @param {string} device - Modem device ID
+ * @param {string} server - Server address to ping
+ * @returns {Promise<number>}
+ */
 export async function pingServerByDevice(device: string, server: string): Promise<number> {
-  // const response = await api.get(`/modem/${device}/ping/${server}`, { timeout: 5000 })
-
-  // return response.data as number
-
-  return Math.floor(Math.random() * 100)
+  const response = await api.get(`/modem/${device}/ping/${server}`, { timeout: 5000 })
+  return response.data as number
 }
 
 export default {
   fetchModems,
   fetchModemByDevice,
+  fetchSignalStrength,
+  fetchServingCellInfo,
+  rebootModem,
+  fetchUSBMode,
+  setUSBMode,
+  fetchPDPInfo,
+  setAPNByProfile,
+  pingServerByDevice,
 }
--- frontend/src/types/ModemManager.ts
+++ frontend/src/types/ModemManager.ts
@@ -1,9 +1,81 @@
-
 /**
  * Represents details about a given modem device
  */
 export interface ModemDevice {
-  device: string;
-  manufacturer: string;
-  product: string;
+  device: string
+  id: string
+  manufacturer: string
+  product: string
+}
+
+/**
+ * Represents information about the serving cell for the modem
+ */
+export interface ServingCellInfo {
+  networkStatus: string
+  accessTechnology: string
+  duplexMode: string
+  mobileCountryCode: number
+  mobileNetworkCode: number
+  cellIdentity: string
+  physicalCellId: number
+  earfcn: number
+  frequencyBand: number
+  trackingAreaCode: number
+  referenceSignalPower: number
+  timingAdvance?: number
+  referenceSignalReceivedPower: number
+  referenceSignalReceivedQuality?: number
+  snr?: number
+  uplinkBandwidth?: number
+  downlinkBandwidth?: number
+}
+
+/**
+ * Represents information about a neighboring cell
+ */
+export interface NeighborCellInfo {
+  cellType: string
+  accessTechnology: string
+  earfcn: number
+  physicalCellId?: number
+  signalQuality?: number
+  referenceSignalReceivedPower?: number
+  snr?: number
+  timingAdvance?: number
+  downlinkBandwidth?: number
+  uplinkBandwidth?: number
+  qrxlevmin?: number
+  signalQualityThreshold?: number
+  signalStrengthThreshold?: number
+}
+
+/**
+ * Represents overall cell information for a modem, including serving and neighboring cells
+ */
+export interface ModemCellInfo {
+  servingCell: ServingCellInfo
+  neighborCells: NeighborCellInfo[]
+}
+
+/**
+ * Represents PDP (Packet Data Protocol) configuration information
+ */
+export interface PDPInfo {
+  profileId: string
+  protocol: string
+  apn: string
+  ipAddress: string
+  primaryDns?: string
+  secondaryDns?: string
+  ipv6Address?: string
+  status: string
+}
+
+/**
+ * Represents signal quality metrics for the modem
+ */
+export interface ModemSignalQuality {
+  signalStrength: number
+  bitErrorRate: number
 }
