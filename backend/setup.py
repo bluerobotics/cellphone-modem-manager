4ef289ea0c1919951d5e911e3ab6a8009fd79b98
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-11-03 13:59:25 -0300

    backend: setup: Fix mess in dep links in setup.py
    * Make all libs compliant with requirements from commonwealth

--- backend/setup.py
+++ backend/setup.py
@@ -15,12 +15,16 @@ setup(
     packages=find_packages(include=['api', 'modem']),
     install_requires=[
         "appdirs==1.4.4",
-        "fastapi==0.115.3",
+        # Starlet is enforced by commonwealth so we need to use the same version
+        "starlette == 0.27.0",
+        "fastapi == 0.105.0",
         "fastapi-versioning==0.10.0",
         "loguru == 0.5.3",
         "pydantic==2.9.2",
         "pyserial==3.5",
         "uvicorn==0.32.0",
-        "commonwealth @ git+https://github.com/bluerobotics/BlueOS.git@1.3.0#egg=commonwealth&subdirectory=core/libs/commonwealth",
+        # This is needed for commonwealth since PIP does not fetch dependency link automatically
+        "pykson @ https://github.com/patrickelectric/pykson/tarball/1.0.2#egg=pykson-1.0.2",
+        "commonwealth @ https://github.com/bluerobotics/BlueOS/archive/refs/tags/1.3.0.tar.gz#subdirectory=core/libs/commonwealth",
     ],
 )
