3f2537741a2e57908c3df2cea5a533c9955880ab
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-11-28 12:11:05 -0300

    backend: cron: Add cron jobs initial impl
    * Change settings to store data usage as dict of month day to data usage
      tuple
    * Add auto configure modem cron
    * Add data usage update cron

--- backend/settings.py
+++ backend/settings.py
@@ -11,14 +11,17 @@ class CellLocationSettings(BaseModel):
 
 
 class DataUsageSettings(BaseModel):
+    def total_data_used(self) -> int:
+        return self.data_used[0] + self.data_used[1]
+
     # Data limit in bytes, default is 2GB
     data_limit: int = 2 * 1024 * 1024 * 1024
     # The day of the month when the data usage resets
     data_reset_day: int = 1
     # RX and TX data used in bytes
     data_used: Tuple[int, int] = (0, 0)
-    # List of data points from last data_reset_day to now
-    data_points: List[Tuple[int, int]] = []
+    # List of data points from last data_reset_day to now, key is the month day and value is RX and TX data used
+    data_points: Dict[int, Tuple[int, int]] = {}
 
 
 class ModemsSettings(BaseModel):
