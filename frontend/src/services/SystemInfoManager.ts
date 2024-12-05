0860c5de7c1262bf38a55752e875beaf68a416e8
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-12-05 13:32:07 -0300

    frontend: Change system info api to correct one

--- frontend/src/services/SystemInfoManager.ts
+++ frontend/src/services/SystemInfoManager.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { NetworkInterface } from '@/types/SystemInfoManager'
 
-const SYSTEM_INFO_API = `/v1.0/blueos/system`
+const SYSTEM_INFO_API = `/v1.0/blueos/system-information`
 
 const api = axios.create({
   baseURL: SYSTEM_INFO_API,
@@ -13,7 +13,7 @@ const api = axios.create({
  * @returns {Promise<NetworkInterface[]>}
  */
 export async function fetchNetworkInfo(): Promise<NetworkInterface[]> {
-  const response = await api.get('/network')
+  const response = await api.get('/system/network')
   return response.data as NetworkInterface[]
 }
 
