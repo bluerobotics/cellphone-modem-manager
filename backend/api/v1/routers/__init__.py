1f080201088eb6826fc870349323c8e1e52a66bc
Joao Mario Lago <joao.mario.lago@hotmail.com>
2026-04-10 16:22:01 -0300

    backend: api: Add report generation endpoints

--- backend/api/v1/routers/__init__.py
+++ backend/api/v1/routers/__init__.py
@@ -3,5 +3,6 @@ from .blueos import blueos_router_v1
 from .index import index_router_v1
 from .modem import modem_router_v1
 from .cells import cells_router_v1
+from .report import report_router_v1
 
-__all__ = ["blueos_router_v1", "cells_router_v1", "index_router_v1", "modem_router_v1"]
+__all__ = ["blueos_router_v1", "cells_router_v1", "index_router_v1", "modem_router_v1", "report_router_v1"]
